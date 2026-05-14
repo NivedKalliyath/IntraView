4ab96b7901bd70cff85c76fd93b09d4e12cf2edc
NivedKalliyath <nivedkalliyath@gmail.com>
2026-05-14 21:25:33 +0400

    minor typo fixes

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -7,12 +7,12 @@ export const protectRoute = [
         try{
             const clerkId = req.auth().userId;
 
-            if(!clerkId) return res.status(401).json({msg: "Unauthorized - Invalid Token"});
+            if(!clerkId) return res.status(401).json({message: "Unauthorized - Invalid Token"});
 
             // find user in db by clerk ID
             const user = await User.findOne({clerkId});
 
-            if(!user) return res.status(404).json({msg: "User not found"});
+            if(!user) return res.status(404).json({message: "User not found"});
 
             // attach user to req
             req.user = user;
@@ -20,7 +20,7 @@ export const protectRoute = [
             next();
         }
         catch(error){
-            console.error("Error in protecRoute middleware",error);
+            console.error("Error in protectRoute middleware",error);
             res.status(500).json({message: "Internal Server Error"});
         }
     },
