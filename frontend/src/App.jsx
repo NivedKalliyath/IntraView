f056391f8ecc83576e94d798d7287de7143d7a8a
NivedKalliyath <nivedkalliyath@gmail.com>
2026-05-16 12:49:06 +0400

    Landing page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,6 @@ import HomePage from "./pages/HomePage";
 
 import { Toaster } from "react-hot-toast";
 import DashboardPage from "./pages/DashboardPage";
-import ProblemPage from "./pages/ProblemPage";
-import ProblemsPage from "./pages/ProblemsPage";
-import SessionPage from "./pages/SessionPage";
 
 function App(){
   const { isSignedIn, isLoaded } = useUser();
@@ -19,10 +16,6 @@ function App(){
       <Routes>
         <Route path="/" element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
         <Route path="/dashboard" element={isSignedIn ? <DashboardPage /> : <Navigate to={"/"} />} />
-
-        <Route path="/problems" element={isSignedIn ? <ProblemsPage /> : <Navigate to={"/"} />} />
-        <Route path="/problem/:id" element={isSignedIn ? <ProblemPage /> : <Navigate to={"/"} />} />
-        <Route path="/session/:id" element={isSignedIn ? <SessionPage /> : <Navigate to={"/"} />} />
       </Routes>
 
       <Toaster toastOptions={{ duration: 3000 }} />
